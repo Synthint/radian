534fff905bb932cf43eb15cf94689dac7dcbd8ab
Sean <seanm0914@gmail.com>
2024-04-04 22:30:59 -0400

    chore: various changes

--- radian/backend/api/api.py
+++ radian/backend/api/api.py
@@ -20,18 +20,47 @@ def delete_file(file, delay):
 
 
 @router.post("/builder/makesolid", response_class=HTMLResponse)
-async def make_solid(num_samples: Annotated[int, Form()],
-                sample_rate: Annotated[int, Form()],
-                sides: Annotated[int, Form()],
-                r_scale: Annotated[int, Form()],
-                core: Annotated[int, Form()],
-                z_scale: Annotated[float, Form()],
-                build_mode: Annotated[str, Form()],
-                file: UploadFile,
-                background_tasks: BackgroundTasks) -> Response:
+async def make_solid(
+    num_samples: Annotated[int, Form()],
+    sample_rate: Annotated[int, Form()],
+    sides: Annotated[int, Form()],
+    r_scale: Annotated[float, Form()],
+    core: Annotated[float, Form()],
+    z_scale: Annotated[float, Form()],
+    build_mode: Annotated[str, Form()],
+    file: UploadFile,
+    background_tasks: BackgroundTasks
+) -> Response:
     
-    print("Building Solid...")
     build_error = ""
+    if num_samples < 10 or num_samples > 16000:
+        build_error = "Number of Samples Not Within Range"
+
+    if sample_rate < 200 or sample_rate > 16000:
+        build_error = "Sample Rate Not Within Range"
+
+    if sides < 3 or sides > 500:
+        build_error = "Sides Not Within Range"
+
+    if r_scale < 0.5 or r_scale > 20:
+        r_scale = "Radial Scale Not Within Range"
+
+    if core < 0.5 or core > 20:
+        core = "Core Diameter Not Within Range"
+
+    if z_scale < 0.05 or z_scale > 5:
+        z_scale = "Height Scale Not Within Range"
+        
+    if build_error == "":
+            ret_msg = f"""
+                <div  class="container"> 
+                    <p>ERROR: {build_error}</p>
+                    <button hx-redirect"/" hx-trigger="click">Reload</button>
+                </div>
+            """
+            return HTMLResponse(content=ret_msg)
+
+    print("Building Solid...")
     try:
         now_time = int(time.time())
         stl_file= f"{file.filename[:-4]}{now_time}.stl"
@@ -40,6 +69,7 @@ async def make_solid(num_samples: Annotated[int, Form()],
 
         with open(file_path, "wb") as f:
             f.write(file.file.read())
+        background_tasks.add_task(delete_file, file_path,180)
         print("Opening Wav File")
         wav = musiclib.read(file_path, sample_limit=num_samples, custom_sample_rate=sample_rate)
         if build_mode == "Cylinder":
@@ -60,6 +90,7 @@ async def make_solid(num_samples: Annotated[int, Form()],
             build_error = "Improper Build Mode"
         cyl: solid = builder.stitch_cylinder(wav_solid, z_scale = z_scale)
         cyl.save_ascii(f"{stl_file}", save_path)
+        background_tasks.add_task(delete_file, save_path,180)
         print(f"solid saved: {stl_file}")
 
         if build_error == "":
@@ -72,18 +103,19 @@ async def make_solid(num_samples: Annotated[int, Form()],
         else:
             ret_msg = f"""
                 <div  class="container"> 
-                    ERROR: {build_error}
+                    <p>ERROR: {build_error}</p>
+                    <p> Please Reload </p>
                 </div>
             """
     except Exception as e:
         print(e)
         ret_msg = f"""
             <div  class="container"> 
-                ERROR: {e}
+                <p>ERROR: {e}</p>
+                <p> Please Reload </p>
             </div>
         """
-    background_tasks.add_task(delete_file, file_path,300)
-    background_tasks.add_task(delete_file, save_path,300)
+
     return HTMLResponse(content=ret_msg)
 
 
